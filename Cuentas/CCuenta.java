0e28563c361dfdb74ed19efa0a6860de81c97fb2
Agustin Glenny <aglenny.uni@gmail.com>
2024-02-16 19:27:43 +0100

    Commit 2

--- Cuentas/CCuenta.java
+++ Cuentas/CCuenta.java
@@ -2,16 +2,28 @@ package Cuentas;
 
 public class CCuenta {
 
-
+    /**
+     * Atributs de la classe CCuenta.
+     */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Constructor buid de la classe CCuenta.
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor de la clase CCuenta.
+     * @param nom El nom del titular del compte.
+     * @param cue El numero del compte.
+     * @param sal Saldo inicial del compte.
+     * @param tipo El tipus.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -19,11 +31,20 @@ public class CCuenta {
         setSaldo(sal);
     }
 
+    /**
+     * Mètode per obtenir l'estat/saldo del compte.
+     * @return El saldo del compte.
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Mètode per ingressar una quantitat al compte.
+     * @param cantidad La quantitat a ingressar.
+     * @throws Exception Si cantidad < 0.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -31,6 +52,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Mètode per retirar una quantitat del compte.
+     * @param cantidad La quantitat a retirar.
+     * @throws Exception Si hi ha errors.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -40,34 +66,66 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+    /**
+     * Mètode per obtenir el nom del titular del compte.
+     * @return El nom del titular del compte.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Mètode per establir el nom del titular del compte.
+     * @param nombre El nou nom del titular del compte.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Mètode per obtenir el número del compte.
+     * @return El número del compte.
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * Mètode per establir el número del compte.
+     * @param cuenta El nou número del compte.
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+    /**
+     * Mètode per obtenir el saldo del compte.
+     * @return El saldo del compte.
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * Mètode per establir el saldo del compte.
+     * @param saldo El nou saldo del compte.
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
+    /**
+     * Mètode per obtenir el tipus d'interès del compte.
+     * @return El tipus d'interès del compte.
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
+    /**
+     * Mètode per definir el tipus d'interès del compte.
+     * @param tipoInterés nou tipus d'interès del compte.
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
